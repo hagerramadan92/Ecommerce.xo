3b6d020f9a6068dbce95bcb01906da1402703544
hageramadan <hagerramadan440@gmail.com>
2025-12-01 13:29:52 +0200

    navbar

--- components/subIcon.tsx
+++ components/subIcon.tsx
@@ -16,10 +16,7 @@ export default function SubIcon({className ,className2 , className3}:SubIconProp
             <span className={`${className2}`}> تتبع الطلب</span>
           </div>
 
-          <div className="flex items-center gap-2  cursor-pointer text-pro-hover hidden3">
-            <BiMessageRoundedEdit size={24} strokeWidth={0.2} className={`${className3}`}/>
-            <span className={`${className2}`}>احصل على عرض سعر</span>
-          </div>
+         
         </div>
         </>
   )
